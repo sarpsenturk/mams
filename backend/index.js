1cff1b80bc01047510339604fd2cb47ce61a60e7
sarpsenturk <sarpsenturk38@gmail.com>
2022-12-22 18:58:17 +0300

    Create express app

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,12 @@
-console.log("Hello world")
+import express from 'express'
+
+// Configuration
+const PORT = process.env.PORT | 8000
+
+// Create the application
+const app = express()
+
+// Listen on specified port
+app.listen(PORT, () => {
+    console.log(`MAMS backend listening on port ${PORT}...`)
+})
